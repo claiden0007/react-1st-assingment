075002646b5e1e06f8ebd0a28d7a1d5b1b488cf9
claiden0007 <138395628+claiden0007@users.noreply.github.com>
2023-09-07 22:35:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,282 @@
+import "./App.css";
+import Video from "./video";
+
+function App() {
+  return (
+    <div className="App">
+      <Video
+        title="Globel News"
+        views="1M"
+        time="4year ago"
+        channel="Globe"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2015/07/28/20/55/tools-864983_1280.jpg"
+            alt="image"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2017/07/27/09/56/sphere-stone-2544690_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Earth News"
+        views="960k"
+        time="2year ago"
+        channel="earth"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_1280.jpg"
+            alt="nature"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2016/02/04/13/49/the-earth-1179212_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Nature News"
+        views="315k"
+        time="1year ago"
+        channel="Wild"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_1280.jpg"
+            alt="Animals"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2017/11/15/20/32/sea-turtle-2952470_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Development News"
+        views="30k"
+        time="1year ago"
+        channel="Bilder"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2020/08/29/10/02/bildings-5526646_1280.jpg"
+            alt="Bildig"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2013/07/12/16/27/safety-helmet-150913_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Educational News"
+        views="100k"
+        time="1year ago"
+        channel="Education"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2014/03/12/18/45/boys-286245_1280.jpg"
+            alt="image"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2016/03/19/04/40/cap-1266204_1280.png"
+            alt="Logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Security"
+        views="111k"
+        time="1year ago"
+        channel="Security"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2019/11/08/10/34/cyber-4610993_1280.jpg"
+            alt="Security"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2016/03/31/17/58/computer-1294045_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Car News"
+        views="70k"
+        time="1year ago"
+        channel="Riders"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2017/03/27/14/56/auto-2179220_1280.jpg"
+            alt="Amg"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2016/04/01/11/10/automobile-1300231_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Business news"
+        views="987k"
+        time="1year ago"
+        channel="Business "
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2017/01/14/10/56/people-1979261_1280.jpg"
+            alt="office"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2016/06/13/15/07/presentation-1454403_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Sky News"
+        views="100k"
+        time="1year ago"
+        channel="coder Dhoni"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_1280.jpg"
+            alt="sky"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2021/04/21/11/59/sun-6196158_1280.png"
+            alt="Logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Art news"
+        views="50k"
+        time="1year ago"
+        channel="Artist"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2013/10/25/20/46/mosaic-200864_1280.jpg"
+            alt="art"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2017/09/13/18/59/floral-2746540_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Party news"
+        views="345k"
+        time="1year ago"
+        channel="Party babe"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2016/11/22/18/56/audience-1850022_1280.jpg"
+            alt="Part"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2013/07/13/13/21/champagne-160867_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Vegetable News"
+        views="800k"
+        time="1year ago"
+        channel="Veg boy"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2016/05/29/23/40/nuts-1424015_1280.jpg"
+            alt="veg"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2012/04/24/16/15/broccoli-40295_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Animi news"
+        views="45k"
+        time="1year ago"
+        channel="animiesss"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2014/10/31/17/41/dancing-dave-minion-510835_1280.jpg"
+            alt="Animi"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2017/08/15/15/44/frog-2644410_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Light News"
+        views="56k"
+        time="1year ago"
+        channel="Light"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2018/01/24/17/33/light-bulb-3104355_1280.jpg"
+            alt="light"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2017/02/01/10/41/candles-2029544_1280.png"
+            alt="Logo"
+          />
+        }
+      ></Video>
+      <Video
+        title="Music News"
+        views="587k"
+        time="1year ago"
+        channel="Tune"
+        thumbnail={
+          <img
+            src="https://cdn.pixabay.com/photo/2014/05/21/15/18/musician-349790_1280.jpg"
+            alt="Music"
+          />
+        }
+        logo={
+          <img
+            src="https://cdn.pixabay.com/photo/2017/04/19/10/24/vinyl-2241789_1280.png"
+            alt="logo"
+          />
+        }
+      ></Video>
+    </div>
+  );
+}
+
+export default App;
+ 
